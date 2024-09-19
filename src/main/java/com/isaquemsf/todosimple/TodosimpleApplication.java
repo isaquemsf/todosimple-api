f1ceba17fbab01ab0df55273642cc7da8acaa04d
isaquemsf <zaque8069@gmail.com>
2024-09-18 21:04:31 -0300

    teste commit1

--- src/main/java/com/isaquemsf/todosimple/TodosimpleApplication.java
+++ src/main/java/com/isaquemsf/todosimple/TodosimpleApplication.java
@@ -8,7 +8,6 @@ public class TodosimpleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodosimpleApplication.class, args);
-		System.out.println("hello world");
 	}
 
 }
